5091a9d1adf465cd51036f55fc6c5cad24760273
Dann Bonderff <brez-danou@hotmail.fr>
2020-05-07 13:56:47 +0200

    add new photographs

--- client/src/app/gallery.service.ts
+++ client/src/app/gallery.service.ts
@@ -26,12 +26,17 @@ export class GalleryService {
 }
 
 const IMAGES =[
-    {"id":1,"urlPreview":"../../assets/images/gallery/preview/DSCF0262.jpg","urlFull":"../../assets/images/gallery/full/DSCF0262.jpg","alt":"photo-DSCF0262"},
-    {"id":2,"urlPreview":"../../assets/images/gallery/preview/DSCF0470.jpg","urlFull":"../../assets/images/gallery/full/DSCF0470.jpg","alt":"photo-DSCF0470"},
-    {"id":3,"urlPreview":"../../assets/images/gallery/preview/DSCF0816.jpg","urlFull":"../../assets/images/gallery/full/DSCF0816.jpg","alt":"photo-DSCF0816"},
-    {"id":4,"urlPreview":"../../assets/images/gallery/preview/DSCF0919.jpg","urlFull":"../../assets/images/gallery/full/DSCF0919.jpg","alt":"photo-DSCF0919"},
-    {"id":5,"urlPreview":"../../assets/images/gallery/preview/DSCF0938.jpg","urlFull":"../../assets/images/gallery/full/DSCF0938.jpg","alt":"photo-DSCF0938"},
-    {"id":6,"urlPreview":"../../assets/images/gallery/preview/DSCF1109.jpg","urlFull":"../../assets/images/gallery/full/DSCF1109.jpg","alt":"photo-DSCF1109"},
+	{"id":12,"urlPreview":"../../assets/images/gallery/preview/DSCF0725.jpg","urlFull":"../../assets/images/gallery/full/DSCF0725.jpg","alt":"photo-DSCF0725"},
+	{"id":11,"urlPreview":"../../assets/images/gallery/preview/DSCF0873-2.jpg","urlFull":"../../assets/images/gallery/full/DSCF0873-2.jpg","alt":"photo-DSCF0873-2"},
+	{"id":10,"urlPreview":"../../assets/images/gallery/preview/DSCF1219-2.jpg","urlFull":"../../assets/images/gallery/full/DSCF1219-2.jpg","alt":"photo-DSCF1219-2"},
+	{"id":9,"urlPreview":"../../assets/images/gallery/preview/DSCF1070.jpg","urlFull":"../../assets/images/gallery/full/DSCF1070.jpg","alt":"photo-DSCF1070"},
+    
+    {"id":8,"urlPreview":"../../assets/images/gallery/preview/DSCF1280.jpg","urlFull":"../../assets/images/gallery/full/DSCF1280.jpg","alt":"photo-DSCF1280"},
     {"id":7,"urlPreview":"../../assets/images/gallery/preview/DSCF1210.jpg","urlFull":"../../assets/images/gallery/full/DSCF1210.jpg","alt":"photo-DSCF1210"},
-	{"id":8,"urlPreview":"../../assets/images/gallery/preview/DSCF1280.jpg","urlFull":"../../assets/images/gallery/full/DSCF1280.jpg","alt":"photo-DSCF1280"}
+    {"id":6,"urlPreview":"../../assets/images/gallery/preview/DSCF1109.jpg","urlFull":"../../assets/images/gallery/full/DSCF1109.jpg","alt":"photo-DSCF1109"},
+    {"id":5,"urlPreview":"../../assets/images/gallery/preview/DSCF0938.jpg","urlFull":"../../assets/images/gallery/full/DSCF0938.jpg","alt":"photo-DSCF0938"},
+    {"id":4,"urlPreview":"../../assets/images/gallery/preview/DSCF0919.jpg","urlFull":"../../assets/images/gallery/full/DSCF0919.jpg","alt":"photo-DSCF0919"},
+    {"id":3,"urlPreview":"../../assets/images/gallery/preview/DSCF0816.jpg","urlFull":"../../assets/images/gallery/full/DSCF0816.jpg","alt":"photo-DSCF0816"},
+    {"id":2,"urlPreview":"../../assets/images/gallery/preview/DSCF0470.jpg","urlFull":"../../assets/images/gallery/full/DSCF0470.jpg","alt":"photo-DSCF0470"},
+    {"id":1,"urlPreview":"../../assets/images/gallery/preview/DSCF0262.jpg","urlFull":"../../assets/images/gallery/full/DSCF0262.jpg","alt":"photo-DSCF0262"},
 ]
